dfcbc451cc8c1a8fc98917622b59ed2e7fda0b18
phroiland <jon.froiland@gmail.com>
2026-06-12 07:08:50 -0500

    Try Haiku for latency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import Script from 'next/script'
 import { Public_Sans } from 'next/font/google'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import './globals.css'
 
 const publicSans = Public_Sans({
